da0b06884a270866598ebf430825e9f41855addd
Roberts Marky <roberts.marky@web.de>
2021-04-21 21:35:47 +0100

    Refactored incidents to suggestions onClickListener setting

--- app/src/main/java/de/tudarmstadt/smartcitystudyapp/ui/incidents/IncidentsFragment.kt
+++ app/src/main/java/de/tudarmstadt/smartcitystudyapp/ui/incidents/IncidentsFragment.kt
@@ -11,6 +11,27 @@ import androidx.navigation.findNavController
 import dagger.hilt.android.scopes.FragmentScoped
 import de.tudarmstadt.smartcitystudyapp.R
 
+val suggestionNavigationSettings = listOf(
+    Triple(R.id.incidents_button_water, R.string.water_heading, R.array.water_array),
+    Triple(R.id.incidents_button_trees, R.string.trees_heading, R.array.trees_array),
+    Triple(R.id.incidents_button_electronic, R.string.electronic_heading, R.array.electronic_array),
+    Triple(R.id.incidents_button_vehicles, R.string.vehicles_heading, R.array.vehicles_array),
+    Triple(R.id.incidents_button_buildings, R.string.buildings_heading, R.array.buildings_array),
+    Triple(R.id.incidents_button_path, R.string.path_heading, R.array.path_array),
+    Triple(R.id.incidents_button_devices, R.string.devices_heading, R.array.devices_array),
+    Triple(R.id.incidents_button_health, R.string.health_heading, R.array.health_array),
+    Triple(R.id.incidents_button_noise, R.string.noise_heading, R.array.noise_array),
+    Triple(R.id.incidents_button_air, R.string.air_heading, R.array.air_array),
+    Triple(R.id.incidents_button_garbage, R.string.garbage_heading, R.array.garbage_array),
+    Triple(R.id.incidents_button_parking, R.string.parking_heading, R.array.parking_array),
+    Triple(R.id.incidents_button_smoke, R.string.smoke_heading, R.array.smoke_array),
+    Triple(R.id.incidents_button_wc, R.string.wc_heading, R.array.wc_array),
+    Triple(R.id.incidents_button_streets, R.string.streets_heading, R.array.streets_array),
+    Triple(R.id.incidents_button_animals, R.string.animals_heading, R.array.animals_array),
+    Triple(R.id.incidents_button_pollution, R.string.pollution_heading, R.array.pollution_array),
+    Triple(R.id.incidents_button_other, R.string.other_heading, R.array.other_array)
+)
+
 @FragmentScoped
 class IncidentsFragment : Fragment() {
     override fun onCreateView(
@@ -22,167 +43,15 @@ class IncidentsFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        view.findViewById<Button>(R.id.incidents_button_water).setOnClickListener {
-            view.findNavController().navigate(
-                R.id.action_incidents_to_suggestion,
-                bundleOf(
-                    "headingStringId" to R.string.water_heading,
-                    "categoryStringId" to R.array.water_array
+        suggestionNavigationSettings.forEach { (buttonId, headingId, arrayId) ->
+            view.findViewById<Button>(buttonId).setOnClickListener {
+                it.findNavController().navigate(
+                    R.id.action_incidents_to_suggestion, bundleOf(
+                        "headingStringId" to headingId,
+                        "categoryStringId" to arrayId
+                    )
                 )
-            )
-        }
-        view.findViewById<Button>(R.id.incidents_button_trees).setOnClickListener {
-            view.findNavController().navigate(
-                R.id.action_incidents_to_suggestion,
-                bundleOf(
-                    "headingStringId" to R.string.trees_heading,
-                    "categoryStringId" to R.array.trees_array
-                )
-            )
-        }
-        view.findViewById<Button>(R.id.incidents_button_electronic).setOnClickListener {
-            view.findNavController().navigate(
-                R.id.action_incidents_to_suggestion,
-                bundleOf(
-                    "headingStringId" to R.string.electronic_heading,
-                    "categoryStringId" to R.array.electronic_array
-                )
-            )
-        }
-        view.findViewById<Button>(R.id.incidents_button_vehicles).setOnClickListener {
-            view.findNavController().navigate(
-                R.id.action_incidents_to_suggestion,
-                bundleOf(
-                    "headingStringId" to R.string.vehicles_heading,
-                    "categoryStringId" to R.array.vehicles_array
-                )
-            )
-        }
-        view.findViewById<Button>(R.id.incidents_button_buildings).setOnClickListener {
-            view.findNavController().navigate(
-                R.id.action_incidents_to_suggestion,
-                bundleOf(
-                    "headingStringId" to R.string.buildings_heading,
-                    "categoryStringId" to R.array.buildings_array
-                )
-            )
-        }
-        view.findViewById<Button>(R.id.incidents_button_path).setOnClickListener {
-            view.findNavController().navigate(
-                R.id.action_incidents_to_suggestion,
-                bundleOf(
-                    "headingStringId" to R.string.path_heading,
-                    "categoryStringId" to R.array.path_array
-                )
-            )
-        }
-        view.findViewById<Button>(R.id.incidents_button_devices).setOnClickListener {
-            view.findNavController().navigate(
-                R.id.action_incidents_to_suggestion,
-                bundleOf(
-                    "headingStringId" to R.string.devices_heading,
-                    "categoryStringId" to R.array.devices_array
-                )
-            )
-        }
-        view.findViewById<Button>(R.id.incidents_button_health).setOnClickListener {
-            view.findNavController().navigate(
-                R.id.action_incidents_to_suggestion,
-                bundleOf(
-                    "headingStringId" to R.string.health_heading,
-                    "categoryStringId" to R.array.health_array
-                )
-            )
-        }
-        view.findViewById<Button>(R.id.incidents_button_noise).setOnClickListener {
-            view.findNavController().navigate(
-                R.id.action_incidents_to_suggestion,
-                bundleOf(
-                    "headingStringId" to R.string.noise_heading,
-                    "categoryStringId" to R.array.noise_array
-                )
-            )
-        }
-        view.findViewById<Button>(R.id.incidents_button_air).setOnClickListener {
-            view.findNavController().navigate(
-                R.id.action_incidents_to_suggestion,
-                bundleOf(
-                    "headingStringId" to R.string.air_heading,
-                    "categoryStringId" to R.array.air_array
-                )
-            )
-        }
-        view.findViewById<Button>(R.id.incidents_button_garbage).setOnClickListener {
-            view.findNavController().navigate(
-                R.id.action_incidents_to_suggestion,
-                bundleOf(
-                    "headingStringId" to R.string.garbage_heading,
-                    "categoryStringId" to R.array.garbage_array
-                )
-            )
-        }
-        view.findViewById<Button>(R.id.incidents_button_parking).setOnClickListener {
-            view.findNavController().navigate(
-                R.id.action_incidents_to_suggestion,
-                bundleOf(
-                    "headingStringId" to R.string.parking_heading,
-                    "categoryStringId" to R.array.parking_array
-                )
-            )
-        }
-        view.findViewById<Button>(R.id.incidents_button_smoke).setOnClickListener {
-            view.findNavController().navigate(
-                R.id.action_incidents_to_suggestion,
-                bundleOf(
-                    "headingStringId" to R.string.smoke_heading,
-                    "categoryStringId" to R.array.smoke_array
-                )
-            )
-        }
-        view.findViewById<Button>(R.id.incidents_button_wc).setOnClickListener {
-            view.findNavController().navigate(
-                R.id.action_incidents_to_suggestion,
-                bundleOf(
-                    "headingStringId" to R.string.wc_heading,
-                    "categoryStringId" to R.array.wc_array
-                )
-            )
-        }
-        view.findViewById<Button>(R.id.incidents_button_streets).setOnClickListener {
-            view.findNavController().navigate(
-                R.id.action_incidents_to_suggestion,
-                bundleOf(
-                    "headingStringId" to R.string.streets_heading,
-                    "categoryStringId" to R.array.streets_array
-                )
-            )
-        }
-        view.findViewById<Button>(R.id.incidents_button_animals).setOnClickListener {
-            view.findNavController().navigate(
-                R.id.action_incidents_to_suggestion,
-                bundleOf(
-                    "headingStringId" to R.string.animals_heading,
-                    "categoryStringId" to R.array.animals_array
-                )
-            )
-        }
-        view.findViewById<Button>(R.id.incidents_button_pollution).setOnClickListener {
-            view.findNavController().navigate(
-                R.id.action_incidents_to_suggestion,
-                bundleOf(
-                    "headingStringId" to R.string.pollution_heading,
-                    "categoryStringId" to R.array.pollution_array
-                )
-            )
-        }
-        view.findViewById<Button>(R.id.incidents_button_other).setOnClickListener {
-            view.findNavController().navigate(
-                R.id.action_incidents_to_suggestion,
-                bundleOf(
-                    "headingStringId" to R.string.other_heading,
-                    "categoryStringId" to R.array.other_array
-                )
-            )
+            }
         }
     }
 }
