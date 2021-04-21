903dce251ad66a067bd601950878c4536e6b37cc
Roberts Marky <roberts.marky@web.de>
2021-04-21 20:50:52 +0100

    Removed ViewModel for the Incidents fragment

--- app/src/main/java/de/tudarmstadt/smartcitystudyapp/ui/incidents/IncidentsFragment.kt
+++ app/src/main/java/de/tudarmstadt/smartcitystudyapp/ui/incidents/IncidentsFragment.kt
@@ -5,10 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import dagger.hilt.android.scopes.FragmentScoped
@@ -16,8 +14,6 @@ import de.tudarmstadt.smartcitystudyapp.R
 
 @FragmentScoped
 class IncidentsFragment : Fragment() {
-    private val incidentsViewModel by viewModels<IncidentsViewModel>()
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
