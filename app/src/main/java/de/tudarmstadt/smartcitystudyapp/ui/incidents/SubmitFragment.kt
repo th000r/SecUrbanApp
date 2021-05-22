a6f5e7a402662af05edf5e97ecb1e7d133af9263
Roberts Marky <roberts.marky@web.de>
2021-05-22 21:46:42 +0100

    Show or hide the 'add picture from gallery' button on switch flip

--- app/src/main/java/de/tudarmstadt/smartcitystudyapp/ui/incidents/SubmitFragment.kt
+++ app/src/main/java/de/tudarmstadt/smartcitystudyapp/ui/incidents/SubmitFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.widget.SwitchCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,9 +24,20 @@ class SubmitFragment : Fragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_submitincidents, container, false)
         val suggestion: String = arguments?.getString("suggestion") ?: ""
+        val galleryButton = root.findViewById<Button>(R.id.incidents_button_gallery)
+        val sendPhotoSwitch = root.findViewById<SwitchCompat>(R.id.switch_send_photo)
+
         root.findViewById<EditText>(R.id.report_text).setText(suggestion)
+
         root.findViewById<Button>(R.id.incidents_button_submit)
             .setOnClickListener { submitViewModel.sendDummyReport(root) }
+
+        sendPhotoSwitch.setOnClickListener {
+            when (sendPhotoSwitch.isChecked) {
+                true -> galleryButton.visibility = View.VISIBLE
+                false -> galleryButton.visibility = View.INVISIBLE
+            }
+        }
         return root
     }
 }
