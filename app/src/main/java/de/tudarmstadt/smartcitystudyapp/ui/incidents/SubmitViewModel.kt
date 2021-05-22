0373747e1f77eaab27659f7d5de576161eb9aa41
Roberts Marky <roberts.marky@web.de>
2021-05-22 15:25:06 +0100

    Attach userId to report

--- app/src/main/java/de/tudarmstadt/smartcitystudyapp/ui/incidents/SubmitViewModel.kt
+++ app/src/main/java/de/tudarmstadt/smartcitystudyapp/ui/incidents/SubmitViewModel.kt
@@ -8,17 +8,21 @@ import androidx.lifecycle.viewModelScope
 import de.tudarmstadt.smartcitystudyapp.R
 import de.tudarmstadt.smartcitystudyapp.model.Report
 import de.tudarmstadt.smartcitystudyapp.services.ReportService
+import de.tudarmstadt.smartcitystudyapp.services.UserService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class SubmitViewModel @ViewModelInject constructor(private val reportService: ReportService) :
+class SubmitViewModel @ViewModelInject constructor(
+    private val reportService: ReportService,
+    private val userService: UserService
+) :
     ViewModel() {
     fun sendDummyReport(view: View) {
         println("Sending dummy report")
         val reportTextField = view.findViewById<EditText>(R.id.report_text)
         viewModelScope.launch(Dispatchers.IO) {
             val report = Report(
-                "42069",
+                userService.getUserId(),
                 reportTextField.text.toString(),
                 picture = false,
                 latitude = 0.0,
