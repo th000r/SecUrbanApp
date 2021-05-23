96e57ca109f0d8fdd644a2c625dd40691f5a3ea0
Roberts Marky <roberts.marky@web.de>
2021-05-23 15:13:26 +0100

    Get location and add to report

--- app/src/main/java/de/tudarmstadt/smartcitystudyapp/model/Report.kt
+++ app/src/main/java/de/tudarmstadt/smartcitystudyapp/model/Report.kt
@@ -1,10 +1,13 @@
 package de.tudarmstadt.smartcitystudyapp.model
 
+const val SOURCE_OTHER = "Eigeninitiative"
+
 data class Report(
     val userId: String,
     val message: String,
-    val latitude: Double,
-    val longitude: Double,
-    val picture: Boolean,
-    val source: String
+    val location: Boolean = false,
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0,
+    val picture: Boolean = false,
+    val source: String = SOURCE_OTHER
 )
--- app/src/main/java/de/tudarmstadt/smartcitystudyapp/ui/incidents/SubmitViewModel.kt
+++ app/src/main/java/de/tudarmstadt/smartcitystudyapp/ui/incidents/SubmitViewModel.kt
@@ -1,20 +1,34 @@
 package de.tudarmstadt.smartcitystudyapp.ui.incidents
 
+import android.Manifest
+import android.content.Context.LOCATION_SERVICE
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.location.LocationManager.GPS_PROVIDER
+import android.os.Build
 import android.os.Looper
+import android.provider.Settings
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.SwitchCompat
+import androidx.core.app.ActivityCompat
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.findNavController
+import com.google.android.gms.location.LocationRequest
+import de.tudarmstadt.smartcitystudyapp.MainActivity
 import de.tudarmstadt.smartcitystudyapp.R
 import de.tudarmstadt.smartcitystudyapp.model.Report
+import de.tudarmstadt.smartcitystudyapp.model.SOURCE_OTHER
 import de.tudarmstadt.smartcitystudyapp.services.ReportService
 import de.tudarmstadt.smartcitystudyapp.services.UserService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.util.function.Consumer
 
 class SubmitViewModel @ViewModelInject constructor(
     private val reportService: ReportService,
@@ -25,18 +39,57 @@ class SubmitViewModel @ViewModelInject constructor(
         println("Sending dummy report") //TODO: Remove debug reports
         viewModelScope.launch(Dispatchers.IO) {
             Looper.myLooper() ?: Looper.prepare()
-            val preToast = Toast.makeText(view.context, R.string.report_prepare_toast, Toast.LENGTH_SHORT)
+            val context = view.context
+            val preToast =
+                Toast.makeText(context, R.string.report_prepare_toast, Toast.LENGTH_SHORT)
             preToast.show()
+            val locationSwitch = view.findViewById<SwitchCompat>(R.id.switch_send_location)
+
+            var locationGiven = false
+            var longitude = 0.0
+            var latitude = 0.0
+
+            if (locationSwitch.isChecked) {
+                val locationService = context.getSystemService(LOCATION_SERVICE) as LocationManager
+                if (!locationService.isProviderEnabled(GPS_PROVIDER)) {
+                    context.startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+                }
+                if (ActivityCompat.checkSelfPermission(
+                        context,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    val permissionToast = Toast.makeText(
+                        context,
+                        R.string.location_permission_missing_toast,
+                        Toast.LENGTH_LONG
+                    )
+                    permissionToast.show()
+                } else {
+                    locationGiven = true
+                    locationService.getCurrentLocation(
+                        GPS_PROVIDER,
+                        null,
+                        context.mainExecutor,
+                        {
+                            longitude = it.longitude
+                            latitude = it.latitude
+                        }
+                    )
+                }
+            }
+
             val report = Report(
                 userId = userService.getUserId() ?: "???",
                 message = view.findViewById<EditText>(R.id.report_text).text.toString(),
                 picture = view.findViewById<SwitchCompat>(R.id.switch_send_photo).isChecked,
-                latitude = 0.0,
-                longitude = 0.0,
-                source = ""
+                location = locationGiven,
+                latitude = latitude,
+                longitude = longitude,
+                source = SOURCE_OTHER
             )
             reportService.sendReport(report)
-            val postToast = Toast.makeText(view.context, R.string.report_sent_toast, Toast.LENGTH_SHORT)
+            val postToast = Toast.makeText(context, R.string.report_sent_toast, Toast.LENGTH_SHORT)
             postToast.show()
             view.findNavController().navigate(R.id.action_global_home)
             println("Sent dummy report $report")
