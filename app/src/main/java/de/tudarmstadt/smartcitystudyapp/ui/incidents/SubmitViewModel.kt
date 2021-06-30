94590c58793c3242ef2278a96651245b40ebd10b
Thorsten Schumacher <>
2021-06-30 15:49:14 +0200

    - fixed bug: toasts were not displayed - added success and error toast - navigation only on success

--- app/src/main/java/de/tudarmstadt/smartcitystudyapp/ui/incidents/SubmitViewModel.kt
+++ app/src/main/java/de/tudarmstadt/smartcitystudyapp/ui/incidents/SubmitViewModel.kt
@@ -1,21 +1,12 @@
 package de.tudarmstadt.smartcitystudyapp.ui.incidents
 
-import android.Manifest
-import android.content.Context.LOCATION_SERVICE
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.location.LocationManager
-import android.location.LocationManager.GPS_PROVIDER
 import android.os.Looper
-import android.provider.Settings
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.widget.SwitchCompat
-import androidx.core.app.ActivityCompat
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.navigation.findNavController
@@ -27,6 +18,7 @@ import de.tudarmstadt.smartcitystudyapp.services.ReportService
 import de.tudarmstadt.smartcitystudyapp.services.UserService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SubmitViewModel @ViewModelInject constructor(
     private val reportService: ReportService,
@@ -39,9 +31,12 @@ class SubmitViewModel @ViewModelInject constructor(
 
         if(MainActivity.network_status == true) {
             println("Sending report") //TODO: Remove debug reports
-            viewModelScope.launch(Dispatchers.IO) {
+            var returnVal = ""
+
+            Toast.makeText(context, R.string.report_prepare_toast, Toast.LENGTH_SHORT).show()
+
+            viewModelScope.launch() {
                 Looper.myLooper() ?: Looper.prepare()
-                Toast.makeText(context, R.string.report_prepare_toast, Toast.LENGTH_SHORT).show()
 
                 val report = Report(
                     userId = userService.getUserId() ?: "???",
@@ -50,9 +45,20 @@ class SubmitViewModel @ViewModelInject constructor(
                     location = view.findViewById<SwitchCompat>(R.id.switch_send_location).isChecked,
                     source = source
                 )
-                reportService.sendReport(report)
-                Toast.makeText(context, R.string.report_sent_toast, Toast.LENGTH_SHORT).show()
-                view.findNavController().navigate(R.id.action_global_home)
+
+                withContext(Dispatchers.IO) {
+                    returnVal = reportService.sendReport(report)
+                    Log.i("sendReport Result", returnVal)
+                }
+
+                withContext(Dispatchers.Main) {
+                    if (!returnVal.contains("Post failed with code")) {
+                        Toast.makeText(context, R.string.report_sent_success_toast, Toast.LENGTH_LONG).show()
+                        view.findNavController().navigate(R.id.action_global_home)
+                    } else {
+                        Toast.makeText(context, R.string.report_sent_error_toast, Toast.LENGTH_LONG).show()
+                    }
+                }
             }
         } else {
             Toast.makeText(context, context.getText(R.string.incidents_submit_connectivity_toast), Toast.LENGTH_LONG).show()
