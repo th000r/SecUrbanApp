7af9af17c7c11c46eecacedee46116bcf96bae45
Thorsten Schumacher <>
2021-08-09 10:02:38 +0200

    redirect to reports

--- app/src/main/java/de/tudarmstadt/smartcitystudyapp/helper/AlarmReceiver.kt
+++ app/src/main/java/de/tudarmstadt/smartcitystudyapp/helper/AlarmReceiver.kt
@@ -8,35 +8,33 @@ import android.content.Intent
 import androidx.core.app.NotificationCompat
 import de.tudarmstadt.smartcitystudyapp.MainActivity
 import de.tudarmstadt.smartcitystudyapp.R
-
-
 /**
- * Created by ptyagi on 4/17/17.
- */
-
-/**
- * Created by ptyagi on 4/17/17.
- */
-/**
- * AlarmReceiver handles the broadcast message and generates Notification
+ * handles the broadcast messages and generates local push notifications
  */
 class AlarmReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         //Get notification manager to manage/send notifications
         //Intent to invoke app when click on notification.
         //In this sample, we want to start/launch this sample app when user clicks on notification
-        val intentToRepeat = Intent(context, MainActivity::class.java)
+        // val intentToRepeat = Intent(context, MainActivity::class.java)
         val id = intent.getIntExtra("id", NotificationHelper.ALARM_TYPE_RTC)
         //set flag to restart/relaunch the app
-        intentToRepeat.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        // intentToRepeat.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
 
+        val intentToRepeat = Intent(context, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_SINGLE_TOP or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            putExtra("fragment", "activity")
+        }
         //Pending intent to handle launch of Activity in intent above
-        val pendingIntent = PendingIntent.getActivity(
-            context,
-            id,
-            intentToRepeat,
-            PendingIntent.FLAG_ONE_SHOT
-        )
+//        val pendingIntent = PendingIntent.getActivity(
+//            context,
+//            id,
+//            intentToRepeat,
+//            PendingIntent.FLAG_ONE_SHOT
+//        )
+
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(context, id, intentToRepeat, PendingIntent.FLAG_UPDATE_CURRENT)
+
 
         //Build notification
         val repeatedNotification: Notification =
@@ -53,10 +51,11 @@ class AlarmReceiver : BroadcastReceiver() {
         intent: Intent?
     ): NotificationCompat.Builder {
         return NotificationCompat.Builder(context!!, "de.tudarmstadt.smartcitystudyapp.channel.REPORT")
-            .setContentIntent(pendingIntent)
             .setSmallIcon(R.drawable.logo_inapp)
             .setContentTitle(intent?.getStringExtra("title"))
             .setContentText(intent?.getStringExtra("message"))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
             .setAutoCancel(true) as NotificationCompat.Builder
     }
 }
